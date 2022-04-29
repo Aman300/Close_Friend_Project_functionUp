c81634d86054481e656adfc12c162f1611d3c0cb
spandey6395 <spandey6395@gmail.com>
2022-04-29 06:13:14 +0530

    Phase2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,27 @@
 const express = require('express');
+
 const router = express.Router();
 
 
 
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
 
-module.exports =  router;
+const AuthorController = require("../Controller/AuthorController")
+router.post("/CreateAuthor", AuthorController.CreateAuthor)
+router.post("/AuthorLogin",AuthorController.Authorlogin)
 
+
+const BlogController = require("../Controller/BlogController")
+router.post("/CreateBlog", BlogController.CreateBlog)
+router.get("/getBlog", BlogController.getBlog)
+router.put("/Updateblogs/:blogId", BlogController.UpdateBlog)
+router.delete("/blogs/:blogId", BlogController.DeleteBlogID)
+router.delete("/DeleteBlogs", BlogController.DeleteBlog)
+
+
+
+
+
+module.exports = router;
